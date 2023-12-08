4fc70a1cea94beedd8c59c6e273454b7144b9dce
unknown <hong.gilseong@gmail.com>
2023-12-08 22:22:29 +0900

    onboard 페이지 폴더구조 작성, 병합 라우트 작성

--- app/(onboard)/layout.tsx
+++ app/(onboard)/layout.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const OnboardLayout = () => {
-  return <div>OnboardLayout</div>;
+type OnboardLayoutProps = {
+  children: ReactNode;
+  modal: ReactNode;
+};
+
+const OnboardLayout = ({ children, modal }: OnboardLayoutProps) => {
+  return (
+    <div className="h-full dark:bg-[#1f1f1f]">
+      {children}
+      {modal}
+    </div>
+  );
 };
 
 export default OnboardLayout;
--- app/(onboard)/page.tsx
+++ app/(onboard)/page.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./_components/navbar";
+
 const OnboardPage = () => {
-  return <div>OnboardPage</div>;
+  return (
+    <div className="flex">
+      <Navbar />
+    </div>
+  );
 };
 
 export default OnboardPage;
