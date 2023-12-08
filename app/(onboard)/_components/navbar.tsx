12c5b7889c7dc6c62426aba8ee2502c43984d206
unknown <hong.gilseong@gmail.com>
2023-12-08 22:21:03 +0900

    navbar 기본 스타일링 작성

--- app/(onboard)/_components/navbar.tsx
+++ app/(onboard)/_components/navbar.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+import Logo from "@/components/logo";
+
+import { useAuthModal } from "@/hooks/use-auth-modal";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const scrolled = useScrollTop();
+
+  const router = useRouter();
+
+  const onOpen = useAuthModal((state) => state.onOpen);
+
+  const handleLogin = () => {
+    onOpen();
+    router.push("/login");
+  };
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-5",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto flex md:justify-end items-center gap-x-2 w-full">
+        <Button variant="outline" onClick={handleLogin}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
