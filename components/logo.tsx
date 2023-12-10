0b07d0cfb316739a0cf1e2362aba3d7ee7777808
unknown <hong.gilseong@gmail.com>
2023-12-10 15:52:16 +0900

    로고 세션에 반응할 수 있게 작성

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,30 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import Image from "next/image";
 
-const Logo = ({ width = 100, height = 60 }) => {
+type LogoProps = {
+  width?: number;
+  height?: number;
+  className?: string;
+};
+
+const Logo = ({ width = 100, height = 60, className }: LogoProps) => {
+  const { status } = useSession();
+
+  const router = useRouter();
   return (
-    <div className="hidden md:flex items-center gap-x-2 cursor-pointer">
+    <div
+      className={cn("flex items-center gap-x-2 cursor-pointer", className)}
+      role="button"
+      onClick={() =>
+        status === "authenticated"
+          ? router.push("/dashboard")
+          : router.push("/")
+      }
+    >
       <Image src="/logo.png" height={height} width={width} alt="logo" />
     </div>
   );
