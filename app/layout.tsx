ac85b7634fb04cd62ec1d66b9f6ecfff24c510bb
unknown <hong.gilseong@gmail.com>
2023-12-11 14:16:07 +0900

    sooner layout 에 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import SessionProvider from "@/components/provider/session-provider";
 
@@ -29,6 +29,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </SessionProvider>
