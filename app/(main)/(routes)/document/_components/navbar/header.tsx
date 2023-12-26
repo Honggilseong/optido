b420b10e402b2b111e6b3ce0404d794db8154f47
unknown <hong.gilseong@gmail.com>
2023-12-26 20:13:28 +0900

    bg color 변경

--- app/(main)/(routes)/document/_components/navbar/header.tsx
+++ app/(main)/(routes)/document/_components/navbar/header.tsx
@@ -14,7 +14,7 @@ const Header = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   const query = useDocument();
   return (
     <>
-      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-2">
+      <nav className="bg-secondary dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-2">
         {isCollapsed && (
           <div className="relative h-6 w-6 group hover:bg-primary/20 rounded-md">
             <MenuIcon className="text-muted-foreground absolute inset-0 w-full h-full group-hover:hidden" />
