292d45ea418b758ddc80e14ac0c5644a54dc3bc9
unknown <hong.gilseong@gmail.com>
2023-12-28 11:52:48 +0900

    search command layout에 추가

--- app/(main)/(routes)/document/layout.tsx
+++ app/(main)/(routes)/document/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./_components/navbar/navbar";
+import SearchCommand from "@/components/search-command";
 
 const DocumentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,6 +8,7 @@ const DocumentLayout = ({ children }: { children: React.ReactNode }) => {
       <Navbar />
 
       <main className="flex-1 h-full overflow-y-auto overflow-x-hidden">
+        <SearchCommand />
         {children}
       </main>
     </div>
