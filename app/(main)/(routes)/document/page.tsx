ffdcae60c7d22a149e4c12bcc66491c61677bb4a
unknown <hong.gilseong@gmail.com>
2023-12-31 09:58:03 +0900

    create document api 연결

--- app/(main)/(routes)/document/page.tsx
+++ app/(main)/(routes)/document/page.tsx
@@ -1,8 +1,34 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { SendHorizontal } from "lucide-react";
+import { useAction } from "@/hooks/use-action";
+import { createDocument } from "@/actions/create-document";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const DocumentPage = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { execute: createExecute } = useAction(createDocument, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["documents"] });
+      toast.success("New document created!");
+      router.push(`/document/${data.id}`);
+    },
+    onError: (error) => {
+      console.log(error);
+
+      toast.error('"Failed to create a new document"');
+    },
+  });
+
+  const onCreateDocument = () => {
+    createExecute({ title: "Untitled" });
+  };
+
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className="flex flex-col justify-center items-center">
@@ -23,6 +49,7 @@ const DocumentPage = () => {
         <Button
           variant="ghost"
           className="dark:bg-muted-foreground/30 max-w-[80%] dark:hover:bg-primary-foreground"
+          onClick={onCreateDocument}
         >
           Create new documents
           <SendHorizontal className="h-4 w-4 ml-2" />
